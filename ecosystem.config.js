6281af04595e42daab57738290afe1f0ee6b4df5
ArmBrsph <burisapholfueangwong@gmail.com>
2024-06-04 16:44:23 +0700

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: 'MINI',
+            name: 'CARFINN_SSR',
             namespace: 'FRONT',
             script: 'npm',
             args: 'run build:ssr',
             autorestart: true,
             max_memory_restart: '1G',
-           // out_file: path.join(__dirname, './src/logs/pm2/') + moment().format('YYYY-MM-DD') + '.log'
         },
     ],
 };
